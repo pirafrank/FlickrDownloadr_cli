acc450268866a2c38172d4399e1f0f8d0e3cc477
Francesco Pira <dev@fpira.com>
2015-05-31 13:22:15 +0200

    license fix

--- flickrdl.py
+++ flickrdl.py
@@ -8,18 +8,18 @@
 #
 # This file is part of FlickrDownloadr_cli
 #
-# This script is free software: you can redistribute it and/or modify
+# FlickrDownloadr_cli is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This script is distributed in the hope that it will be useful,
+# FlickrDownloadr_cli is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
+# along with FlickrDownloadr_cli. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
